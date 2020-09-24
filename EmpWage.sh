07ad5df9558a947c0eee8419a536721f5094761d
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-24 19:43:51 +0530

    five commit

--- EmpWage.sh
+++ EmpWage.sh
@@ -5,6 +5,7 @@ echo "Welcome"
 #CONSTANTS
 IS_FULL_TIME=2
 IS_PART_TIME=1
+MAX_WORKING_DAYS=20;
 EMP_RATE_PER_HR=20
 
 isPresent=1
@@ -16,3 +17,15 @@ then
 else
 	echo "Employee is Abscent"
 fi
+
+case $randomcheck in
+	1) $IS_FULL_TIME
+		workHrs=8
+		;;
+	2) $IS_PART_TIME
+		workHrs=4
+		;;
+	*)
+		workHrs=0
+		;;
+esac
