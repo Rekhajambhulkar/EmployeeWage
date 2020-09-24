8b5248be150a9e0b42acb0747fc40ca3f8bf2262
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-24 18:45:38 +0530

    second commit

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,3 +1,13 @@
 #! /bin/bash -x
 
 echo "Welcome"
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+
+if [ $randomCheck -eq $isPresent ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is Abscent"
+fi
